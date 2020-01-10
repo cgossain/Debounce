365a649ee218cea238d2207dccb704e0d73e969e
Christian Gossain <cgossain@gmail.com>
2020-01-09 18:09:05 -0700

    Initial commit

---
+++ Example/Debounce/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Debounce
+//
+//  Created by cgossain on 01/09/2020.
+//  Copyright (c) 2020 cgossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
