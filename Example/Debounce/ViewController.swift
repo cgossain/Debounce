0e4f494f1f70492809b88f18e610359acbde13b0
Christian Gossain <cgossain@gmail.com>
2021-01-25 22:04:27 -0700

    Implement barebones example using UISearchController
    This example illustrates how to debouce search results, however the underlying Throttler class can be used in many other scenarios.
    
    I also updated pods on the example project in this commit.

--- Example/Debounce/ViewController.swift
+++ Example/Debounce/ViewController.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import Debounce
 
 class ViewController: UIViewController {
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    private let searchThrottler = SearchThrottler()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.autocapitalizationType = .none
+        
+        // search controller uses the search throttler as its search results
+        // updater, and we'll set ourselves as the `throttledSearchResultsUpdater`
+        // to receive the throttled callbacks
+        searchController.searchResultsUpdater = searchThrottler
+        searchThrottler.throttledSearchResultsUpdater = self
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print("This is a throttle call to update search results")
+    }
+}
